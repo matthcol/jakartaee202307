e2e5a18d47f3c47f12d7cb9d54b5c0e37a48a172
matthcol <matthias.colin@gmail.com>
2023-07-31 09:49:12 +0200

    fix test in template

--- template/src/test/java/org/example/SampleTest.java
+++ template/src/test/java/org/example/SampleTest.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SampleTest {
 
+	@Test
     void aTest(){
         assertEquals(4, 2*2);
     }
