82367ec139ca642ec550a12d5534f822cae49c8f
matthcol <matthias.colin@gmail.com>
2023-08-02 14:25:09 +0200

    svae JPA demo

--- moviejpa/src/test/java/org/example/entity/MovieCrudDemo.java
+++ moviejpa/src/test/java/org/example/entity/MovieCrudDemo.java
@@ -2,11 +2,25 @@ package org.example.entity;
 
 import jakarta.persistence.EntityManager;
 import org.example.bootstrap.JpaBootstrap;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MovieCrudDemo {
+
+    static EntityManager entityManager;
+
+    @BeforeAll
+    static void bootstrapJpa() {
+        entityManager = JpaBootstrap.createEntityManager();
+    }
+
+    @AfterAll
+    static void shutdownJpa() {
+        entityManager.close();
+    }
 
     @Test
     void demoSave() {
@@ -14,6 +28,11 @@ class MovieCrudDemo {
                 .title("The Covenant")
                 .year((short) 2023)
                 .build();
+        entityManager.getTransaction().begin();
+        entityManager.persist(movie); // INSERT here or later
+        entityManager.flush();
+        System.out.println(movie);
+        entityManager.getTransaction().commit();
     }
 
 }
