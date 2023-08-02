329a7f5248fb0569d02a026d3e2c1484944feeb8
matthcol <matthias.colin@gmail.com>
2023-08-02 15:20:57 +0200

    read JPA

--- moviejpa/src/test/java/org/example/entity/MovieCrudDemo.java
+++ moviejpa/src/test/java/org/example/entity/MovieCrudDemo.java
@@ -2,12 +2,14 @@ package org.example.entity;
 
 import jakarta.persistence.EntityManager;
 import org.example.bootstrap.JpaBootstrap;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(OrderAnnotation.class)
 class MovieCrudDemo {
 
     static EntityManager entityManager;
@@ -19,12 +21,14 @@ class MovieCrudDemo {
 
     @AfterAll
     static void shutdownJpa() {
-        //        entityManager.close();
         JpaBootstrap.closeEntityManager(entityManager);
     }
 
     @Test
+    @Order(1)
     void demoSave() {
+        System.out.println();
+        System.out.println("*** Demo Save with Hibernate ***");
         var movie = Movie.builder()
                 .title("The Covenant")
                 .year((short) 2023)
@@ -34,6 +38,18 @@ class MovieCrudDemo {
         entityManager.flush();
         System.out.println(movie);
         entityManager.getTransaction().commit();
+    }
+
+    @Test
+    @Order(2)
+    void demoReadById() {
+        System.out.println();
+        System.out.println("*** Demo Read by id with Hibernate ***");
+        // movie is null if not found
+        Movie movie = entityManager.find(Movie.class, 1);
+        // NB: Spring => Optional<Movie>
+        Optional<Movie> optMovie = Optional.ofNullable(movie);
+        System.out.println(optMovie);
     }
 
 }
