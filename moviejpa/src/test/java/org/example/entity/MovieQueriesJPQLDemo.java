1088cd770ede7f8060a7a454cb98918f4b2e1f6e
matthcol <matthias.colin@gmail.com>
2023-08-03 14:13:38 +0200

    more queries

--- moviejpa/src/test/java/org/example/entity/MovieQueriesJPQLDemo.java
+++ moviejpa/src/test/java/org/example/entity/MovieQueriesJPQLDemo.java
@@ -6,6 +6,10 @@ import jakarta.persistence.Persistence;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.text.MessageFormat;
 
 /**
  * Doc:
@@ -36,7 +40,7 @@ public class MovieQueriesJPQLDemo {
     void demoFindAll(){
         String queryJPQL = "SELECT m FROM Movie m";
         var movies = entityManager.createQuery(queryJPQL, Movie.class)
-                .getResultList(); // execute SQL
+                .getResultList(); // execute SQL and store results in a List<Movie>
         System.out.println("Movie count: " + movies.size());
         movies.stream()
                 .skip(200)
@@ -46,6 +50,43 @@ public class MovieQueriesJPQLDemo {
                     System.out.println("\t- title = " + movie.getTitle());
                     System.out.println("\t- year = " + movie.getYear());
                 });
+    }
+
+    @Test
+    void demoFindAllByYear(){
+        String queryJPQL = "SELECT m FROM Movie m WHERE m.year = :year";
+        short year = 1984;
+        entityManager.createQuery(queryJPQL, Movie.class)
+                .setParameter("year", year)
+                .getResultStream() // execute SQL query
+                .forEach(System.out::println);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1984,Gremlins",
+            "1934,The Man Who Knew Too Much",
+            "1956,The Man Who Knew Too Much",
+            "1984,B%"
+    })
+    void demoFindAllByYearAndTitle(short year, String title){
+        String queryJPQL = """
+                SELECT
+                    m
+                FROM
+                    Movie m
+                WHERE
+                    m.year = :year
+                    AND m.title LIKE :title
+        """;
+
+        System.out.println(MessageFormat.format(" - Find movies with title like {0} and year = {1} -",
+                title, year));
+        entityManager.createQuery(queryJPQL, Movie.class)
+                .setParameter("year", year)
+                .setParameter("title", title)
+                .getResultStream() // execute SQL query
+                .forEach(System.out::println);
     }
 
 }
